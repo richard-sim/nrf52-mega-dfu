09fba7e5af6b3d6b25bed8b63becb3969013a558
Tom Keddie <github@bronwenandtom.com>
2024-04-17 17:44:12 -0700

    Add pin toggle to megadfu-bootstrap

--- megadfu-bootstrap/main.c
+++ megadfu-bootstrap/main.c
@@ -1,12 +1,19 @@
 #include "bootloader_util.h"
 
+#include "nrf_gpio.h"
 
 #if !defined(MEGADFU_START)
 #error MEGADFU_START must be defined!
 #endif
 
+#define GPIO_OUTPUT_PIN_NUMBER 22
 
 int main(void) {
+
+  nrf_gpio_cfg_output(GPIO_OUTPUT_PIN_NUMBER);
+  nrf_gpio_pin_toggle(GPIO_OUTPUT_PIN_NUMBER);
+  nrf_gpio_pin_toggle(GPIO_OUTPUT_PIN_NUMBER);
+
 	// Just forward on to MegaDFU, which is located at an offset that won't be obliterated by the incoming SoftDevice update
 	bootloader_util_app_start(MEGADFU_START);
 
