760318b0f28406f3205084987ff0f065c3062895
Tom Keddie <github@bronwenandtom.com>
2024-09-22 15:52:06 -0700

    Add prx_nvmc_page_erase

--- megadfu-bootstrap/main.c
+++ megadfu-bootstrap/main.c
@@ -7,6 +7,24 @@
 #error MEGADFU_START must be defined!
 #endif
 
+static void prx_nvmc_page_erase(uint32_t address)
+{
+    // Enable erase.
+    NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Een;
+    __ISB();
+    __DSB();
+
+    // Erase the page
+    NRF_NVMC->ERASEPAGE = address;
+    wait_for_flash_ready();
+
+    NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Ren;
+    __ISB();
+    __DSB();
+}
+
+
+
 int main(void) {
   // S132 version 2.0 occupies 0000_0000-0001_b6b0
   // S132 version 7.2 occupies 0000_0000-0002_596c
