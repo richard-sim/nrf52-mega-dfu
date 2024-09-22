2d233ffef9a70a1083624da229c533ff9ce4bcbf
Tom Keddie <github@bronwenandtom.com>
2024-09-22 15:39:17 -0700

    Try UICR erase

--- megadfu-bootstrap/main.c
+++ megadfu-bootstrap/main.c
@@ -1,3 +1,6 @@
+#include "nrf.h"
+#include <nrf52.h>
+
 #include "bootloader_util.h"
 
 #if !defined(MEGADFU_START)
@@ -10,6 +13,10 @@ int main(void) {
   // SDK11 will place this code at 0001_c000 which will be overwritten when the softdevice is updated
   // Here we use the booloader to start the updater application at
   // MEGADFU_START (0x0002_6000) which is above S132 version 7.2
+  if (NRF_UICR->APPROTECT != 0xffffffff) {
+	  prx_nvmc_page_erase((uint32_t)NRF_UICR);
+	  NVIC_SystemReset();
+  }
   bootloader_util_app_start(MEGADFU_START);
 
   return 0;
